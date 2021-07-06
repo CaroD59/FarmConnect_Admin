8f6f24d03dc789bf78cd73c84d4ccb684f8da412
AntoineCau <acauwd@gmail.com>
2021-07-06 19:41:10 +0200

    adding Users component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import { Admin, Resource } from 'react-admin';
 import dataProvider from './dataProvider';
-import authProvider from './authProvider';
-import CharactersList from './components/Characters/List';
-import CharactersEdit from './components/Characters/Edit';
-import CharactersPost from './components/Characters/Post';
+import UsersList from './components/Users/List';
+import UsersEdit from './components/Users/Edit';
+import UsersPost from './components/Users/Post';
 
 function App() {
   return (
     <div className="App">
-      <Admin authProvider={authProvider} dataProvider={dataProvider}>
+      <Admin dataProvider={dataProvider}>
         <Resource
-          name="characters"
-          list={CharactersList}
-          edit={CharactersEdit}
-          create={CharactersPost}
+          name="users"
+          list={UsersList}
+          edit={UsersEdit}
+          create={UsersPost}
         />
       </Admin>
     </div>
